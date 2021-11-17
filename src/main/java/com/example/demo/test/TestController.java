c65a65a1f7af0d90da22e553d098938f0b69b6dc
guogaoqin <67480337@qq.com>
2021-11-17 22:48:26 +0800

    提交代码

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-
         return "我是菜鸡，你不是，哈哈哈哈哈哈....success!";
     }
 }
