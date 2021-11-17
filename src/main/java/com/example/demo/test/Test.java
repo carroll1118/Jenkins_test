26cfae99c075a45840672e7f25d682f459fcad39
guogaoqin <67480337@qq.com>
2021-11-17 14:41:45 +0800

    提交代码

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo.test;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+
+public class Test {
+    @Getter(name = "/test")
+    public void print() {
+        System.out.println("-----------------测试----------");
+    }
+}
