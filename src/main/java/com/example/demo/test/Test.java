62c39ffa5b8c97efe1d4167b0f09d9bac3cb9d5f
guogaoqin <67480337@qq.com>
2021-11-17 14:46:27 +0800

    提交代码

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -6,6 +6,7 @@ import jdk.nashorn.internal.objects.annotations.Getter;
 public class Test {
     @Getter(name = "/test")
     public void print() {
-        System.out.println("-----------------测试----------");
+        System.out.println("-----------------测试1----------");
+        System.out.println("-----------------测试2----------");
     }
 }
