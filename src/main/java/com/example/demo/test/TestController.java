a77859739c27794df6425ef675dec7727e9dea3d
guogaoqin <67480337@qq.com>
2021-11-18 23:07:31 +0800

    提交代码

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -7,6 +7,7 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         System.out.println("我只是为了练习git操作！！！");
+        System.out.println("我只是为了练习git操作！！！");
         return "我是菜鸡，你不是，哈哈哈哈哈哈....success!";
     }
 }
